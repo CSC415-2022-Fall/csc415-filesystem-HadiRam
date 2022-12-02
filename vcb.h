97c3455a4b7c43c39c569b84117d3bf2f252a161
Manish Sharma <mscchapagain@gmail.com>
2022-12-01 17:10:29 -0800

    adding standard header

--- vcb.h
+++ vcb.h
@@ -1,3 +1,16 @@
+/**************************************************************
+* Class:  CSC-415-01 Fall 2022
+* Names: Kimheng Peng, Hadi Ramlaoui, Manish Sharma, Rajan Shrestha
+* Student IDs: 921847378, 922155413, 922220894, 922223741
+* GitHub Name: TotifyKH, HadiRam, mscchapagain, rajshrestha015
+* Group Name: File Explorers
+* Project: Basic File System
+*
+* File: b_io.c
+*
+* Description: 
+*
+**************************************************************/
 #ifndef VCB_H
 #define _VCB_H
 //Size of VCB is 24 bytes
